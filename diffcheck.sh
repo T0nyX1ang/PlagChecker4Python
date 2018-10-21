0663fe8d23eecaeee23bd939cff896a8414231a2
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2018-10-21 19:33:52 +0800

    Update diffcheck.sh

--- diffcheck.sh
+++ diffcheck.sh
@@ -1,11 +1,17 @@
-mkdir COMPARE_TESTS;
+# Check if working directory exists.
+if [ ! -d "COMPARE_TESTS" ]; then
+	mkdir COMPARE_TESTS;
+fi
+
+# Check if working directory writable. (Later development)
+
 for i in `ls -1`; do
 	for j in `ls -1`; do
 		if [ "$i" != "$j" ] && [ "$i" != "diffcheck.sh" ] && [ "$j" != "diffcheck.sh" ] && [ "$i" != "COMPARE_TESTS" ] && [ "$j" != "COMPARE_TESTS" ]; then
 			s="$i""_COMPARE_""$j"".txt"; 
 			d="COMPARE_TESTS"
 			cd "$d";
-			diff -aicEZbwBIy "../""$i" "../""$j" > "$s";
+			diff -C 0 -aiEZbwBIy "../""$i" "../""$j" > "$s";
 			cd "..";
 			echo "[compare] $i $j OK!"
 		fi
