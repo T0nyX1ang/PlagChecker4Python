55dd600c26077e85b134d59b6b6e9987a317dc21
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2018-10-21 22:57:05 +0800

    Update diffcheck.sh
    Check lowest version of code plagiarism: just copy, or add annotations, spaces, blank lines or case-sensitive changes.

--- diffcheck.sh
+++ diffcheck.sh
@@ -1,19 +1,32 @@
-# Check if working directory exists.
-if [ ! -d "COMPARE_TESTS" ]; then
+WORKING_DIR=".";
+TESTING_DIR="COMPARE_TESTS";
+
+# Check if testing directory exists.
+if [[ ! -d "$TESTING_DIR" ]]; then
 	mkdir COMPARE_TESTS;
 fi
 
-# Check if working directory writable. (Later development)
+# Check if working and testing directory writable.
+if [[ ! -w "$TESTING_DIR" ]] || [[ ! -w "$WORKING_DIR" ]]; then
+	# Output an error if permission denied.
+	echo "[Error] Permission Denied! Check your permission first.";
+	exit;
+fi
 
 for i in `ls -1`; do
 	for j in `ls -1`; do
-		if [ "$i" != "$j" ] && [ "$i" != "diffcheck.sh" ] && [ "$j" != "diffcheck.sh" ] && [ "$i" != "COMPARE_TESTS" ] && [ "$j" != "COMPARE_TESTS" ]; then
-			s="$i""_COMPARE_""$j"".txt"; 
-			d="COMPARE_TESTS"
-			cd "$d";
-			diff -C 0 -aiEZbwBIy "../""$i" "../""$j" > "$s";
+		if [[ "$i" < "$j" ]] && [[ "$i" != "diffcheck.sh" ]] && [[ "$j" != "diffcheck.sh" ]] && [[ "$i" != "$TESTING_DIR" ]] && [[ "$j" != "$TESTING_DIR" ]]; then
+			COMPARE_FILE="$i""_COMPARE_""$j"".txt";
+			cd "$TESTING_DIR";
+			# Suppressing all white spaces(-w), blank lines(-B), sensitive cases(-i) and annotations(-I '^#').
+			# Making an unified 'diff' output here.
+			diff -w -B -i -I '^#' -u "../""$i" "../""$j" > "$COMPARE_FILE";
+			echo "[Info] Stage 1: Checking if two files are identical."
+			if [[ ! -s "$COMPARE_FILE" ]]; then
+				echo "[Info] Plagiarism detected. Identical files found!";
+			fi
 			cd "..";
-			echo "[compare] $i $j OK!"
+			echo "[Info] Compare $i $j OK!"
 		fi
 	done
 done
