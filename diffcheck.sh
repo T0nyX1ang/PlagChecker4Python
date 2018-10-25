6b04de6dfbaea9ede3d835f02c2a8dc6bf6ef989
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2018-10-25 13:16:02 +0800

    Update diffcheck.sh

--- diffcheck.sh
+++ diffcheck.sh
@@ -1,5 +1,7 @@
 WORKING_DIR=".";
 TESTING_DIR="COMPARE_TESTS";
+ACCEPT_DATA_LIMIT=6500;
+REJECT_LINE_LIMIT=50;
 
 # Check if testing directory exists.
 if [[ ! -d "$TESTING_DIR" ]]; then
@@ -17,16 +19,44 @@ for i in `ls -1`; do
 	for j in `ls -1`; do
 		if [[ "$i" < "$j" ]] && [[ "$i" != "diffcheck.sh" ]] && [[ "$j" != "diffcheck.sh" ]] && [[ "$i" != "$TESTING_DIR" ]] && [[ "$j" != "$TESTING_DIR" ]]; then
 			COMPARE_FILE="$i""_COMPARE_""$j"".txt";
+
+			echo "[Info] Evaluating file sizes of $i.";
+			ORIG_FILE_1_SIZE=`du -h -b "$i"`;
+			FILE_1_SIZE=${ORIG_FILE_1_SIZE%%	*};
+			
+			echo "[Info] Evaluating file sizes of $i.";
+			ORIG_FILE_2_SIZE=`du -h -b "$j"`;
+			FILE_2_SIZE=${ORIG_FILE_2_SIZE%%	*};
+			
+			echo "[Info] Comparing $i and $j.";
 			cd "$TESTING_DIR";
 			# Suppressing all white spaces(-w), blank lines(-B), sensitive cases(-i) and annotations(-I '^#').
 			# Making an unified 'diff' output here.
 			diff -w -B -i -I '^#' -u "../""$i" "../""$j" > "$COMPARE_FILE";
-			echo "[Info] Stage 1: Checking if two files are identical."
+			
+			echo "[Info] Stage 1: Checking if two files are identical.";
+
 			if [[ ! -s "$COMPARE_FILE" ]]; then
 				echo "[Info] Plagiarism detected. Identical files found!";
+				cd "..";
+				continue;
 			fi
+			
+			echo "[Info] Stage 2: Checking if the difference of those files are above limit.";
+
+			ORIG_COMPARE_SIZE=`du -h -b "$COMPARE_FILE"`;
+			COMPARE_SIZE=${ORIG_COMPARE_SIZE%%	*};
+
+			if [ $COMPARE_SIZE -ge $ACCEPT_DATA_LIMIT ]; then
+				echo "[Info] Comparing $i $j OK!";
+				echo "[Info] Deleting compare files.";
+				rm "$COMPARE_FILE";
+				echo "[Info] Deleting OK!";
+				cd "..";
+				continue;
+			fi
+			
 			cd "..";
-			echo "[Info] Compare $i $j OK!"
 		fi
 	done
 done
