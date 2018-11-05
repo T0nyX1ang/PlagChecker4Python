34b8e87aefcccc84ef5ae40901339a090baf9c63
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2018-11-05 17:17:57 +0800

    Update diffcheck.sh

--- diffcheck.sh
+++ diffcheck.sh
@@ -1,11 +1,11 @@
 WORKING_DIR=".";
 TESTING_DIR="COMPARE_TESTS";
 ACCEPT_DATA_LIMIT=300;
-REJECT_LINE_LIMIT=50;
+REJECT_LINE_SCORE=80;
 
 # Check if testing directory exists.
 if [[ ! -d "$TESTING_DIR" ]]; then
-	mkdir COMPARE_TESTS;
+	mkdir $TESTING_DIR;
 fi
 
 # Check if working and testing directory writable.
@@ -15,8 +15,8 @@ if [[ ! -w "$TESTING_DIR" ]] || [[ ! -w "$WORKING_DIR" ]]; then
 	exit;
 fi
 
-if [[ ! -s "log.txt" ]]; then
-	rm "log.txt";
+if [[ -f "$TESTING_DIR/log.txt" ]]; then
+	rm "$TESTING_DIR/log.txt";
 fi
 
 for i in `ls -1 | grep .py$ `; do
@@ -27,10 +27,16 @@ for i in `ls -1 | grep .py$ `; do
 			echo "[Info] Evaluating file sizes of $i.";
 			ORIG_FILE_1_SIZE=`du -h -b "$i"`;
 			FILE_1_SIZE=${ORIG_FILE_1_SIZE%%	*};
+
+			echo "[Info] Evaluating file lines of $i.";
+			FILE_1_LINES=`grep ^[a-zA-Z0-P_=" "\*\&\(\)\$"	"] "$i" | wc -l`;
 			
 			echo "[Info] Evaluating file sizes of $j.";
 			ORIG_FILE_2_SIZE=`du -h -b "$j"`;
 			FILE_2_SIZE=${ORIG_FILE_2_SIZE%%	*};
+
+			echo "[Info] Evaluating file lines of $j."
+			FILE_2_LINES=`grep ^[a-zA-Z0-P_=" "\*\&\(\)\$"	"] "$j" | wc -l`;
 			
 			echo "[Info] Comparing $i and $j.";
 			cd "$TESTING_DIR";
@@ -42,6 +48,7 @@ for i in `ls -1 | grep .py$ `; do
 
 			if [[ ! -s "$COMPARE_FILE" ]]; then
 				echo "[Info] Plagiarism detected. Identical files found!";
+				echo "[Info] Plagiarism detected. Identical files found!" >> "log.txt";
 				cd "..";
 				continue;
 			fi
@@ -51,7 +58,7 @@ for i in `ls -1 | grep .py$ `; do
 			ORIG_COMPARE_SIZE=`du -h -b "$COMPARE_FILE"`;
 			COMPARE_SIZE=${ORIG_COMPARE_SIZE%%	*};
 
-			((DATA_LIMIT=$FILE_1_SIZE+$FILE_2_SIZE-$COMPARE_SIZE+$ACCEPT_DATA_LIMIT))
+			((DATA_LIMIT=$FILE_1_SIZE+$FILE_2_SIZE-$COMPARE_SIZE+$ACCEPT_DATA_LIMIT));
 
 			if [ $DATA_LIMIT -le 0 ]; then
 				echo "[Info] Comparing $i $j OK!";
@@ -65,7 +72,10 @@ for i in `ls -1 | grep .py$ `; do
 			echo "[Info] Stage 3: Checking those files line by line.";
 			FILE_1_DIFFERENT_LINES=`grep ^[-][a-zA-Z0-P_=" "\*\&\(\)\$"	"] "$COMPARE_FILE" | wc -l`;
 			FILE_2_DIFFERENT_LINES=`grep ^[+][a-zA-Z0-P_=" "\*\&\(\)\$"	"] "$COMPARE_FILE" | wc -l`;
-			if [ $FILE_1_DIFFERENT_LINES -ge $REJECT_LINE_LIMIT ] || [ $FILE_2_DIFFERENT_LINES -ge $REJECT_LINE_LIMIT ]; then
+			((FILE_1_SCORE=$FILE_1_DIFFERENT_LINES*100/FILE_1_LINES));
+			((FILE_2_SCORE=$FILE_2_DIFFERENT_LINES*100/FILE_2_LINES));
+
+			if [ $FILE_1_SCORE -ge $REJECT_LINE_SCORE ] || [ $FILE_2_SCORE -ge $REJECT_LINE_SCORE ]; then
 				echo "[Info] Comparing $i $j OK!";
 				echo "[Info] Deleting compare files.";
 				rm "$COMPARE_FILE";
@@ -77,10 +87,10 @@ for i in `ls -1 | grep .py$ `; do
 			echo "[Info] Suspected plagiarism detected. Different lines lower than the limit.";
 			echo "[Info] Saving file digest to log.";
 			echo "-------- File Digest Separator --------" >> "log.txt";
-			echo "#1 Filename: $i::Different lines: $FILE_1_DIFFERENT_LINES." >> "log.txt";
-			echo "#2 Filename: $j::Different lines: $FILE_2_DIFFERENT_LINES." >> "log.txt";
+			echo "#1 Filename: $i::File #1 lines: $FILE_1_LINES::Different lines: $FILE_1_DIFFERENT_LINES::Score = $FILE_1_SCORE." >> "log.txt";
+			echo "#2 Filename: $j::File #2 lines: $FILE_2_LINES::Different lines: $FILE_2_DIFFERENT_LINES::Score = $FILE_2_SCORE." >> "log.txt";
 			echo "" >> "log.txt";
-			echo "[Preserving difference file for further investigation.]";
+			echo "[Info] Preserving difference file for further investigation.";
 			
 			#sleep 2;
 
@@ -88,5 +98,4 @@ for i in `ls -1 | grep .py$ `; do
 		fi
 	done
 done
-
 
