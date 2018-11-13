27b722dad23412f8a7a23daf242374d08b19b259
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2018-11-13 18:28:10 +0800

    Update diffcheck.sh

--- diffcheck.sh
+++ diffcheck.sh
@@ -6,14 +6,13 @@ readonly CGrn="\e[0;32m";
 readonly CYel="\e[0;33m";
 readonly CClr="\e[0m";
 
-# Define help function
+# Define help and message function
 function print_help() {
 	# Print help 
 	echo -e "Plagiarism Checker for Python";
 	echo -e "A lightweight tool to detect structure of code plagiarism.";
 	echo -e "Written by Tony Xiang.\n";
-	echo -e "Usage: ./diffcheck.sh [parameters]\n";
-	echo -e "The parameters should follow like this: -verbosity[-q/u/v] -strictness[-l/n/s/S=0-100] \n";
+	echo -e "Usage: ./diffcheck.sh -q/u/v(verbosity) -l/n/s/S=0-100(strictness) \n";
 	echo -e "Verbosity options:";
 	echo -e "  -q Quiet mode. Will not generate messages. Only summary and error message will be shown.";
 	echo -e "  -u Urgent-only mode. Will generate urgent(Detected/Suspected/Error) messages. [Default]";
@@ -31,6 +30,7 @@ function print_message() {
 	case $1 in
 		error )
 			echo -e "${CRed}[Error] $2$CClr";
+			print_help;
 			;;
 		detected )
 			if [[ "$target_mode" != "q" ]]; then
@@ -55,7 +55,7 @@ function print_message() {
 
 # Parameters and syntax check. Latest parameters will override old ones.
 TARGET_MODE="u";
-REJECT_LINE_SCORE=-1;
+REJECT_LINE_SCORE=70;
 for param in $@; do
 	case "${param#*-}" in
 		h* )
@@ -78,133 +78,52 @@ for param in $@; do
 			if [ -n "$temp" -a "$temp" = "${temp//[^0-9]/}" ] && [ $temp -ge 0 ] && [ $temp -le 100 ]; then
 				REJECT_LINE_SCORE=$temp;
 			else
-				print_message error "Invalid syntax or format in inputting scores.\n";
-				print_help;
+				print_message error "Invalid syntax or format in inputting scores. Please see the help below\n";
 			fi
 			;;
 		* )
 			print_message error "Wrong parameter. Please see the help below.\n";
-			print_help;
 			;;
 	esac
 done
-
-if [ $REJECT_LINE_SCORE == -1 ]; then
-	REJECT_LINE_SCORE=70;
-fi
-
-# Configuration for working environment.
-WORKING_DIR=".";
-TESTING_DIR="COMPARE_TESTS";
-ACCEPT_DATA_LIMIT=300;
-SUMMARY_LINE="Summary of files: ";
-
-# Check if testing directory exists.
-if [[ ! -d "$TESTING_DIR" ]]; then
-	print_message info "Creating testing directory.";
-	mkdir $TESTING_DIR;
-fi
-
-# Check if working and testing directory writable.
-if [[ ! -w "$TESTING_DIR" ]] || [[ ! -w "$WORKING_DIR" ]]; then
-	# Output an error if permission denied.
-	print_message error "Permission Denied! Check your permission first.";
-	exit;
-fi
-
-# Check log file validity.
-if [[ -f "$TESTING_DIR/log.txt" ]]; then
-	print_message info "Deleting existing log file.";
-	rm "$TESTING_DIR/log.txt";
-fi
 
 # Basic stats.
 TOTAL_FILES=`ls -1 -l | grep ^- | wc -l`;
 VALID_FILES=`ls -1 | grep .py$ | wc -l`;
 ((CHECKED_PAIRS=$VALID_FILES*($VALID_FILES-1)/2));
 FATAL_PAIRS=0;
+SUMMARY_LINE="Summary of files: ";
 
 # Main process goes here.
 for i in `ls -1 | grep .py$ `; do
 	for j in `ls -1 | grep .py$ `; do
 		if [[ "$i" < "$j" ]]; then
-			COMPARE_FILE="$i""_COMPARE_""$j"".txt";
-			
 			# Step 1
-			print_message info "Evaluating file sizes of $i.";
-			ORIG_FILE_1_SIZE=`du -h -b "$i"`;
-			FILE_1_SIZE=${ORIG_FILE_1_SIZE%%	*};
-			print_message info "Evaluating file lines of $i.";
-			FILE_1_LINES=`grep ^[a-zA-Z0-P_=" "\*\&\(\)\$"	"] "$i" | wc -l`;
-			print_message info "Evaluating file sizes of $j.";
-			ORIG_FILE_2_SIZE=`du -h -b "$j"`;
-			FILE_2_SIZE=${ORIG_FILE_2_SIZE%%	*};
-			print_message info "Evaluating file lines of $j.";
-			FILE_2_LINES=`grep ^[a-zA-Z0-P_=" "\*\&\(\)\$"	"] "$j" | wc -l`;
-			
+			print_message info "Evaluating file lines of $i and $j";
+			FILE_1_LINES=`grep ^[a-zA-Z0-9_=" "\*\&\(\)\$"	"] "$i" | wc -l`;
+			FILE_2_LINES=`grep ^[a-zA-Z0-9_=" "\*\&\(\)\$"	"] "$j" | wc -l`;
+
 			print_message info "Comparing $i and $j.";
-			print_message info "Stage 1: Checking if two files are identical.";
-			cd "$TESTING_DIR";
+			print_message info "Stage 1: Checking those files line by line.";
 			# Suppressing all white spaces(-w), blank lines(-B), sensitive cases(-i) and annotations(-I '^#').
-			# Making an unified 'diff' output here.
-			diff -w -B -i -I '^#' -u "../""$i" "../""$j" > "$COMPARE_FILE";
-
-			if [[ ! -s "$COMPARE_FILE" ]]; then
-				print_message detected "Plagiarism detected. Identical files found!";
-				print_message info "Saving file digest to log.";
-				echo -e "Plagiarism detected. Identical files found!" >> "log.txt";
-				echo -e "-------- File Digest Separator --------" >> "log.txt";
-				echo -e "#1 Filename: $i::File #1 lines: $FILE_1_LINES::Different lines: 0::Score = 0." >> "log.txt";
-				echo -e "#2 Filename: $j::File #2 lines: $FILE_2_LINES::Different lines: 0::Score = 0." >> "log.txt";
-				echo -e "" >> "log.txt";
-				cd "..";
-				((FATAL_PAIRS=$FATAL_PAIRS+1))
-				continue;
-			fi
-			
-			# Step 2
-			print_message info "Stage 2: Checking if the difference of those files are above limit.";
-			ORIG_COMPARE_SIZE=`du -h -b "$COMPARE_FILE"`;
-			COMPARE_SIZE=${ORIG_COMPARE_SIZE%%	*};
-			((DATA_LIMIT=$FILE_1_SIZE+$FILE_2_SIZE-$COMPARE_SIZE+$ACCEPT_DATA_LIMIT));
-
-			if [ $DATA_LIMIT -le 0 ]; then
-				print_message info "Comparing $i $j OK!";
-				rm "$COMPARE_FILE";
-				print_message info "Deleting compare files.";
-				print_message info "Deleting OK!";
-				cd "..";
-				continue;
-			fi
-			
-			# Step 3
-			print_message info "Stage 3: Checking those files line by line.";
-			FILE_1_DIFFERENT_LINES=`grep ^[-][a-zA-Z0-9_=" "\*\&\(\)\$"	"] "$COMPARE_FILE" | wc -l`;
-			FILE_2_DIFFERENT_LINES=`grep ^[+][a-zA-Z0-9_=" "\*\&\(\)\$"	"] "$COMPARE_FILE" | wc -l`;
+			# Making an unified 'diff' output here. Use pipes to connect file streams.
+			FILE_1_DIFFERENT_LINES=`diff -w -B -i -I '^#' -u "$i" "$j" | grep ^[-][a-zA-Z0-9_=" "\*\&\(\)\$"	"] | wc -l`;
+			FILE_2_DIFFERENT_LINES=`diff -w -B -i -I '^#' -u "$i" "$j" | grep ^[+][a-zA-Z0-9_=" "\*\&\(\)\$"	"] | wc -l`;
 			((FILE_1_SCORE=$FILE_1_DIFFERENT_LINES*100/FILE_1_LINES));
 			((FILE_2_SCORE=$FILE_2_DIFFERENT_LINES*100/FILE_2_LINES));
 
 			if [ $FILE_1_SCORE -ge $REJECT_LINE_SCORE ] || [ $FILE_2_SCORE -ge $REJECT_LINE_SCORE ]; then
 				print_message info "Comparing $i $j OK!";
-				rm "$COMPARE_FILE";
-				print_message info "Deleting compare files.";
-				print_message info "Deleting OK!";
-				cd "..";
 				continue;
+			elif [[ $FILE_1_SCORE == 0 ]]; then
+				print_message detected "Code plagiarism detected. Two files are identical.";
+			else
+				print_message suspected "Suspected plagiarism detected. Different lines lower than the limit.";
 			fi
-
-			print_message suspected "Suspected plagiarism detected. Different lines lower than the limit.";
-			print_message info "Saving file digest to log.";
-			echo -e "-------- File Digest Separator --------" >> "log.txt";
-			echo -e "#1 Filename: $i::File #1 lines: $FILE_1_LINES::Different lines: $FILE_1_DIFFERENT_LINES::Score = $FILE_1_SCORE." >> "log.txt";
-			echo -e "#2 Filename: $j::File #2 lines: $FILE_2_LINES::Different lines: $FILE_2_DIFFERENT_LINES::Score = $FILE_2_SCORE." >> "log.txt";
-			echo -e "" >> "log.txt";
-			print_message info "Preserving difference file for further investigation.";
 			((FATAL_PAIRS=$FATAL_PAIRS+1))
 
-			# Step 4
-			print_message info "Stage 4: Comparing last modified time.";
-			cd "..";
+			# Step 2
+			print_message info "Stage 2: Comparing last modified time.";
 			if [[ `find $i -newer $j` == $i ]]; then
 				SUMMARY_LINE=$SUMMARY_LINE"\nSuspected/Detected plagiarism: ${CGrn}$j$CClr->${CRed}$i$CClr [$FILE_2_SCORE:$FILE_1_SCORE].";
 			else
@@ -216,7 +135,7 @@ for i in `ls -1 | grep .py$ `; do
 done
 
 print_message info "Clearing screen ... ";
-sleep 3;
+sleep 1.5;
 clear;
 echo -e "------ SUMMARY ------";
 echo -e "${VALID_FILES} valid files out of ${TOTAL_FILES} files.";
